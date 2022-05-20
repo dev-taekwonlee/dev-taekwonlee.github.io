9f1b0e1a9e1bb58416a2528c1c90ca244efe697c
dev-taekwonlee <dev.taekwonlee@gmail.com>
2022-05-20 14:21:02 +0900

    modified 저는 영화를 좋아해요 in main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let target = document.querySelector("#dynamic");
 
 function randomString() {
-	let stringArr= ["이태권입니다.", "위코드 34기입니다.", "국사학을 전공했어요!", "여행을 좋아해요.", "녹차를 좋아해요.", "저는 영화를 좋아해요."];
+	let stringArr= ["이태권입니다.", "위코드 34기입니다.", "국사학을 전공했어요!", "여행을 좋아해요.", "녹차를 좋아해요.", "영화를 좋아해요."];
 	let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
 	let selectStringArr = selectString.split("");
 
